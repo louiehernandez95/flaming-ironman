89e4a0fc81cfe2a9612e445e12fd6ca27c213266
Rhernandez513 <rhernandez513@gmail.com>
2015-02-03 08:07:44 -0600

    I couldn't get it to compile :(
    it's 8am, i worked through the night, brain is fried.
    
    attempted to create a class and object to separate the if statement
    logic and couldnt get it to work (attached as alt version)
    
    attempted to simplify the code
    
    tried using string variable assignments on the if statements, could not
    figure out how to compile on Xamarin (was looking for an end-of-file
    signal)
    
    Had an issue with Xamarin and namespaces
    
    New tester is simply another file i was using to test bits of code
    without editing the original, is likely reverted already
    
    I'm gonna hit the rack, work on what you can, ill see you in class

--- new  2 tester.cs
+++ new  2 tester.cs
@@ -1,55 +1,48 @@
-using System;
-
-namespace test.cs
-{
-public class LetterGrade
-{ 
-    int grade = 94;
-    static string GradeLetterMsg()
-    {  
-		public static void Main()
-		{
-			if (grade == 100){
-				Console.WriteLine (GradeLetter("an A+ "));
+public class LetterGradeMsg
+			{
+				public static void Main()
+				{
+					string GradeLetter;
+						if (grade == 100){
+							Console.WriteLine (GradeLetter = ("an A+ "));
+						}
+						else if (grade >= 93) {
+							Console.WriteLine (GradeLetter = ("an A "));
+						}  
+						else if (grade >= 90) {
+							Console.WriteLine (GradeLetter = ("an A- "));
+						}
+						else if (grade >= 87) {
+							Console.WriteLine (GradeLetter = ("a B+ "));
+						}
+						else if (grade >= 83) {
+							Console.WriteLine (GradeLetter = ("a B "));
+						}
+						else if (grade >= 80) {
+							Console.WriteLine (GradeLetter = ("a B- "));
+						}
+						else if (grade >= 77) {
+							Console.WriteLine (GradeLetter = ("a C+ "));
+						}
+						else if (grade >= 73) {
+							Console.WriteLine (GradeLetter = ("a C "));
+						}
+						else if (grade >= 70) {
+							Console.WriteLine (GradeLetter = ("a C- "));
+						}
+						else if (grade >= 67) {
+							Console.WriteLine (GradeLetter = ("a D+ "));
+						}
+						else if (grade >= 63) {
+							onsole.WriteLine (GradeLetter = ("a D "));
+						}              
+						else if (grade >= 60) {
+							Console.WriteLine (GradeLetter = ("a D- "));
+						}
+						else if (grade < 60){
+							Console.WriteLine (GradeLetter = ("so low that you are unlikely to pass "));
+						}
+				}				
 			}
-			else if (grade >= 93) {
-				Console.WriteLine (GradeLetter("an A "));
-			}  
-			else if (grade >= 90) {
-				Console.WriteLine (GradeLetter("an A- "));
-			}
-			else if (grade >= 87) {
-			Console.WriteLine (GradeLetter("a B+ "));
-			}
-			else if (grade >= 83) {
-            Console.WriteLine (GradeLetter("a B "));
-			}
-			else if (grade >= 80) {
-            Console.WriteLine (GradeLetter("a B- "));
-			}
-			else if (grade >= 77) {
-            Console.WriteLine (GradeLetter("a C+ "));
-			}
-			else if (grade >= 73) {
-            Console.WriteLine (GradeLetter("a C "));
-			}
-			else if (grade >= 70) {
-            Console.WriteLine (GradeLetter("a C- "));
-			}
-			else if (grade >= 67) {
-            Console.WriteLine (GradeLetter("a D+ "));
-			}
-			else if (grade >= 63) {
-            Console.WriteLine (GradeLetter("a D "));
-			}              
-			else if (grade >= 60) {
-			Console.WriteLine (GradeLetter("a D- "));
-			}
-			else if (grade < 60){
-			Console.WriteLine (GradeLetter("so low that you are unlikely to pass "));
-			}
-		}
-		LetterGrade = ConsoleReadLine();
-        Console.WriteLine("Your grade is " + LetterGrade);
-   }
-}
+			LetterGrade = ConsoleReadLine();
+			Console.WriteLine(@"Your grade is " + LetterGradeMsg);
