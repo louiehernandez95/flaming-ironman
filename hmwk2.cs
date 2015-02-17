8f7a89fc93a627d3d08f0d22781b2cf2ccb970da
Rhernandez513 <rhernandez513@gmail.com>
2015-02-17 13:30:49 -0600

    V0.1 hmwk2

--- hmwk2.cs
+++ hmwk2.cs
@@ -1,16 +1,38 @@
 ﻿using System;
 using IntroCS;
-using quotient;
 
-namespace MainName
+namespace gradecalc
 {
    class MainClass
    {
       public static void Main (string[] args)
       {
+        //int categoryNumber = UI.PromptInt("Please enter the number of grades in the category exam: ");
+
+        int examAverage = FindAverage ("Please enter the number of grades in the exam category: ");
+      }
+
+      public static int FindAverage(string userprompt)
+      {
+        Console.WriteLine(userprompt);
+        int numberOfGrades = int.Parse(Console.ReadLine());
+
+        int i = 1;
+        int totalScore = 0;
+        do{
+          int score = UI.PromptInt("Please enter the grade of assignment " + i + ": ");
+          i++;
+          totalScore += score;
+        } while (i < numberOfGrades);
+        return totalScore / numberofGrades;
+      }
+
+      /*
+      {
          Console.WriteLine ("Hello World!");
          string hello = UI.PromptLine("Enter hello: ");
          Console.WriteLine(hello);
       }
+      */
    }
 }
