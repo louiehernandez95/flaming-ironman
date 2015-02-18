8a98cf1c36e5f012d1a982286142ad9be973c882
Rhernandez513 <rhernandez513@gmail.com>
2015-02-17 18:00:58 -0600

    added a couple of functions and weight gatherer

--- hmwk2.cs
+++ hmwk2.cs
@@ -6,39 +6,52 @@ namespace IntroCS
    {
       public static void Main (string[] args)
       {
-        Console.WriteLine("This Calculator will help you determine a grade in a given class.");
+        Console.WriteLine("This Calculator will help you determine a grade in a given class.\n");
+        getWeights();
         getAverages();
       }
 
+      public static void getWeights()
+      {
+        Console.WriteLine("Begin by entering individual category weights, they must add to 100%\n");
+        double examWeight = FindWeight("exam");
+        double projectWeight = FindWeight("project");
+        double labWeight = FindWeight("lab");
+        double homeworkWeight = FindWeight("homework");
+        double participationWeight = FindWeight("participation point");
+        double weightSum = (examWeight + projectWeight + labWeight + homeworkWeight + participationWeight);
 
+        if (weightSum == 100) {
+           Console.WriteLine ("Congratulations! Weights add to 100%\n");
+        } else {Console.WriteLine("Sorry weights did not add to 100%, please try again.\n");
+            return;
+          }
+      }
+      public static double FindWeight(string weightPrompt)
+      {
+        double weight = UI.PromptDouble("Please enter the weight of " + weightPrompt + "(s): ");
+        return weight;
+      }
       public static void getAverages()
       {
-        int examAverage = FindAverage("exam");
-        int projectAverage = FindAverage("project");
-        int labAverage = FindAverage("lab");
-        int homeworkAverage = FindAverage("homework assignment");
-        int participationAverage = FindAverage ("participation point");
+        double examAverage = FindAverage("exam");
+        double projectAverage = FindAverage("project");
+        double labAverage = FindAverage("lab");
+        double homeworkAverage = FindAverage("homework assignment");
+        double participationAverage = FindAverage ("participation point");
 
       }
-      public static int FindAverage(string gradePrompt)
+      public static double FindAverage(string gradePrompt)
       {
-        int numberOfGrades = UI.PromptInt("Please enter the number of " + gradePrompt +  "(s) to grade: ");
-        int i = 1;
-        int totalScore = 0;
+        double numberOfGrades = UI.PromptDouble("Please enter the number of " + gradePrompt +  "(s) to grade: ");
+        double i = 1;
+        double totalScore = 0;
          do{
-          int score = UI.PromptInt("Please enter the grade of assignment " + i + ": ");
+          double score = UI.PromptDouble("Please enter the grade of assignment " + i + ": ");
           i++;
           totalScore += score;
         } while (i <= numberOfGrades);
          return totalScore / numberOfGrades;
       }
-
-      /*
-      {
-         Console.WriteLine ("Hello World!");
-         string hello = UI.PromptLine("Enter hello: ");
-         Console.WriteLine(hello);
-      }
-      */
    }
 }
