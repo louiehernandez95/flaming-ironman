50be6f726c3615bc330e1db208a543a815944ef7
Rhernandez513 <rhernandez513@gmail.com>
2015-02-19 02:44:32 -0600

    got the first 2 arrays to work!!!
    compiles, not finish, almost

--- hmwk2.cs
+++ hmwk2.cs
@@ -9,28 +9,30 @@ namespace IntroCS
       Console.WriteLine("This Calculator will help you determine a grade in a " + "given class.\n");
       getWeights();
       getAverages();
-      double finalGrade = GiveFinalGrade(double examAverage, double examWeight, double projectAverage, double projectWeight, double labAverage, double labWeight, double homeworkAverage, double homeworkWeight, double participationAverage, double participationWeight);
-      string GradeLetter = GradeAssignment(finalGrade);
+      //GiveFinalGrade(double examAverage, double examWeight, double projectAverage, double projectWeight, double labAverage, double labWeight, double, homeworkAverage, double homeworkWeight, double participationAverage, double participationWeight);
+      //string GradeLetter = GradeAssignment(finalGrade);
     }
 
     //prompts user for weights, tests if adds to 100, loops back if test fails
-    public static void getWeights()
+    public static double[] getWeights()
     {
       Console.WriteLine("Begin by entering individual category weights, they " + "must add to 100%\n");
       double weightSum = 0;
+      double[] weightValue = new double[5];
       do{
-        double examWeight = FindWeight("exam");
-        double projectWeight = FindWeight("project");
-        double labWeight = FindWeight("lab");
-        double homeworkWeight = FindWeight("homework");
-        double participationWeight = FindWeight("participation point");
-        weightSum = (examWeight + projectWeight + labWeight + homeworkWeight + participationWeight);
+        weightValue[0] = FindWeight("exam");
+        weightValue[1] = FindWeight("project");
+        weightValue[2] = FindWeight("lab");
+        weightValue[3] = FindWeight("homework");
+        weightValue[4] = FindWeight("participation point");
+        weightSum = (weightValue[0] + weightValue[1] + weightValue[2] + weightValue[3] + weightValue[4]);
         if (weightSum == 100) {
           Console.WriteLine ("Congratulations! Weights add to 100%\n");
         } else{
             Console.WriteLine("Sorry weights did not add to 100%, please try " + "again.\n");
           }
       }while(weightSum != 100);
+      return weightValue;
     }
 
     //prompts user for weights of each category
@@ -40,14 +42,17 @@ namespace IntroCS
       return weight;
     }
 
+
     //gets and assigns average grades of each category
-    public static void getAverages()
+    public static double[] getAverages()
     {
-      double examAverage = FindAverage("exam");
-      double projectAverage = FindAverage("project");
-      double labAverage = FindAverage("lab");
-      double homeworkAverage = FindAverage("homework assignment");
-      double participationAverage = FindAverage ("participation point");
+      double[] averageValue = new double [5];
+      averageValue[0] = FindAverage("exam");
+      averageValue[1] = FindAverage("project");
+      averageValue[2] = FindAverage("lab");
+      averageValue[3] = FindAverage("homework assignment");
+      averageValue[4] = FindAverage ("participation point");
+      return averageValue;
     }
 
     //prompts for grade and averages the scores for passed in categories
@@ -115,9 +120,11 @@ namespace IntroCS
         GradeLetter = ". It is so low that you are unlikely to pass ";
       }
       return GradeLetter;
+
+
       /*
       LetterGradeMsg = GradeLetter;
-			grade = Math.Round(grade, 2);	//CAM couldn't get the technique from the book to work, found this other technique
+			grade = Math.Round(grade, 2);
 			Console.WriteLine("Your grade is " + grade + LetterGradeMsg);
       */
     }
